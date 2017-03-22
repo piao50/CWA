8468b974b9fe17e1d1ff80b9f62bdb1b3f9ec2f2
gushi2017 <gushi@nuctech.com>
2017-03-22 13:29:00 +0800

    Update cwa_spi.h

--- spi/cwa_spi.h
+++ spi/cwa_spi.h
@@ -1,6 +1,10 @@
 #ifndef CWA_SPI_H
 #define CWA_SPI_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
 #ifdef STM32
 /* SPI_Clock_Polarity */
 // #define SPI_CPOL_Low                    ((uint16_t)0x0000)
@@ -10,7 +14,6 @@
 // #define SPI_CPHA_2Edge                  ((uint16_t)0x0001)
 #endif
 
-
 #ifdef LINUX
 // #define	SPI_CPHA	0x01			/* clock phase */
 // #define	SPI_CPOL	0x02			/* clock polarity */
@@ -19,5 +22,9 @@
 // #define	SPI_MODE_2	(SPI_CPOL|0)
 // #define	SPI_MODE_3	(SPI_CPOL|SPI_CPHA)
 #endif
+   
+#ifdef __cplusplus
+}
+#endif   
 
 #endif
