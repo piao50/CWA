77d3e3de7d39779848fcb1ccbcca4befe897b0b4
piao50 <piao50@gmail.com>
2017-04-10 01:58:09 -0700

    gushi

--- io/keys.c
+++ io/keys.c
@@ -1,9 +1,42 @@
-// gcc keys.c -o keys
-#include "keys.h"
-#include <stdio.h>
+/* A Simple GPIO application
+*  Written by Derek Molloy for the book "Exploring Raspberry Pi" */
 
-int main(int argc, char** argv)
-{
-  printf("%s\n", "Hello, keys!\n");
-  return 0;
+#include <iostream>
+#include <unistd.h>  // for the usleep() function
+#include "GPIO.h"
+#include "keys.h"
+using namespace exploringRPi;
+using namespace std;
+
+
+int main(){
+   GPIO out[5] = {GPIO(LED1), GPIO(LED2), GPIO(LED3), GPIO(LED4), GPIO(LED5)};
+   GPIO in[3] = {GPIO(BTN1), GPIO(BTN2),GPIO(BTN3)};
+
+   for(int i = 0; i < 5; i++)
+   	out[i].setDirection(OUTPUT);
+   for(int i = 0; i < 3; i++)
+   	inGPIO.setDirection(INPUT);
+
+   for (int i=0; i<100; i++){        // flash the LED 10 times
+   		for(int j = 0; j < 5; j++)
+      		out[i].setValue(HIGH);       // turn the LED on
+      usleep(50000);               // sleep for 0.5 seconds
+   		for(int j = 0; j < 5; j++)
+      		out[i].setValue(LOW);       // turn the LED on
+      usleep(50000);               // sleep for 0.5 seconds
+   }
+
+
+
+   // inGPIO.setDirection(INPUT);      // basic input example
+   // cout << "The input state is: "<< inGPIO.getValue() << endl;
+
+   // outGPIO.streamOpen();            // fast write example
+   // for (int i=0; i<1000000; i++){   // write 1 million cycles
+   //    outGPIO.streamWrite(HIGH);    // high
+   //    outGPIO.streamWrite(LOW);     // immediately low, repeat
+   // }
+   // outGPIO.streamClose();           // close the stream
+   return 0;
 }
--- io/keys.h
+++ io/keys.h
@@ -1,5 +1,14 @@
-#ifndef _KEYS_H
-#define _KEYS_H
+#ifndef KEYS_H_
+#define KEYS_H_
 
+#define LED1 26
+#define LED2 19
+#define LED3 13
+#define LED4 6
+#define LED5 5
+
+#define BTN1 21
+#define BTN2 20
+#define BTN3 16
 
 #endif
